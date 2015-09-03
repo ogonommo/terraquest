bcb24075cab64f79f232e28fe08023cc141cc5f6
ogonommo <ogonommo@gmail.com>
2015-09-03 11:13:06 +0300

    Some small changes.

--- server/controllers/planetController.js
+++ server/controllers/planetController.js
@@ -12,6 +12,25 @@ module.exports = {
 		});
 	},
 	updatePlanet: function(effect) {
-
+		Planet.findOne({}).exec(function(err, p){
+			if (err) {
+				console.log('Error getting planet ' + err);
+				return;
+			}
+			if (effect) {
+				for (var key in effect) {
+					if (typeof effect[key] !== undefined) {
+						//console.log('in planet, affecting key ' + key + ' with value ' + effect[key]/100);
+						p[key] = p[key] + effect[key]/100;
+					}
+				}
+				p.save(function(err){
+					if (err) {
+						console.log('Error saving planet data ' + err);
+						return;
+					}
+				})
+			}
+		});
 	}
 }
